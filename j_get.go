9ce6c7cb4724c95fbb3b5f81a69f359a20efe0c8
Jerry <jerry@iguiyu.com>
2019-03-13 17:00:28 +0800

    fix

--- j_get.go
+++ j_get.go
@@ -37,12 +37,12 @@ func get(this *procedure, beanValue reflect.Value) (err error) {
 	fmt.Println("elem:", elem)
 	numField := elem.NumField() //结构体中字段个数
 	elemType := elem.Type()     //结构体类型
+	var column string
 	for i := 0; i < numField; i++ {
 		field := elemType.Field(i) //遍历每一个字段
 		fieldName := field.Name
 		fieldType := field.Type
 		tag := field.Tag.Get("jorm") //获取jorm的tag
-		var column string
 		if tag != null {
 			column = tag
 		} else {
--- jorm_test.go
+++ jorm_test.go
@@ -7,7 +7,10 @@ package jorm
 
 import (
 	"fmt"
+	"gitlab.iguiyu.com/park/struct/model"
+	"log"
 	"testing"
+	"time"
 )
 
 /*
@@ -34,20 +37,63 @@ type Contact struct {
 }
 
 func TestCallProcedure(t *testing.T) {
-	err := InitMySQL("root:Ming521.@tcp(jerry.igoogle.ink:3306)/db_test?charset=utf8")
-
+	//err := InitMySQL("root:Ming521.@tcp(jerry.igoogle.ink:3306)/db_test?charset=utf8")
+	err := InitMySQL("developer:Iloveguiyu2018!@tcp(rm-uf6sl3y5zl5mku48jho.mysql.rds.aliyuncs.com:3306)/lock_test?charset=utf8")
 	if err != nil {
 		fmt.Println("err:", err)
 	}
+	session := MySQL().NewSession()
 
 	//contact := new(Contact)
 	//columns := []string{"name", "age", "phone_number", "home_address"}
 	//
-	//err = MySQL().Where("name = ?", "付明明").Cols(columns...).Find(contact)
+	//_, err = session.Where("name = ?", "付明明").Cols(columns...).Get(contact)
 	//if err != nil {
-	//	fmt.Println("err:", err)
+	//	fmt.Println("err2:", err)
 	//} else {
 	//	fmt.Println("contact:", contact)
+	//}
+	//
+	//sql, i := session.LastSQL()
+	//log.Println("sql:", sql)
+	//log.Println("i:", i)
+
+	vip := new(model.SiteVip)
+	has, err := session.Table("site_vip").Where("plate_number = ?", "沪HZ5690").
+		And("start_time <= ?", time.Now().Format("2006-01-02 15:04:05")).Get(vip)
+	if err != nil {
+		log.Println("err:", err)
+	}
+	log.Println("has:", has)
+	log.Println(vip)
+
+	//poleLog := new(model.PoleLog)
+	//_, err = session.Table("pole_log").Where("plate_number = ?", "沪HZ5690").Desc("id").Get(poleLog)
+	//if err != nil {
+	//	errorLog := new(model.ErrorLog)
+	//	errorLog.Project = "测试项目"
+	//	errorLog.Method = "TestCallProcedure"
+	//	errorLog.Param = "sadfds"
+	//	sql, value := session.LastSQL()
+	//	errorLog.ErrorSql = fmt.Sprintf("%v : %v", sql, value)
+	//	errorLog.ErrotMsg = err.Error()
+	//	errorLog.CreateTime = time.Now()
+	//	//写入数据库
+	//	session.Table("error_log").InsertOne(errorLog)
+	//}
+	//log.Println("poleLog:", poleLog)
+
+	//log := new(model.PoleLog)
+	////_, err = MySQL().ID(172134).Get(log)
+	//_, err = MySQL().ID(14).Get(log)
+	//if err != nil {
+	//	fmt.Println("err:", err)
+	//}
+	//
+	//if log.EndTime.IsZero() {
+	//	fmt.Println("endtime: null")
+	//} else {
+	//	fmt.Println("endtime:", log.EndTime)
 	//}
 
 	//result, err := CallProcedure("query_student", 1, 9).InParams("付明明").Query()
@@ -64,12 +110,27 @@ func TestCallProcedure(t *testing.T) {
 	//	fmt.Println("err:", err)
 	//}
 	//fmt.Println("contact:", contact)
+	//
+	//contactList := make([]*Contact, 0)
+	//err = CallProcedure("query_student", 1, 9).InParams("付明明").Find(&contactList)
+	//if err != nil {
+	//	fmt.Println("err:", err)
+	//}
+	//fmt.Println("contactList:", contactList)
 
-	contactList := make([]*Contact, 0)
-	err = CallProcedure("query_student", 1, 9).InParams("付明明").Find(&contactList)
+}
+
+func TestDbSearch(t *testing.T) {
+	err := InitMySQL("developer:Iloveguiyu2018!@tcp(rm-uf6sl3y5zl5mku48jho.mysql.rds.aliyuncs.com:3306)/lock_test?charset=utf8")
 	if err != nil {
 		fmt.Println("err:", err)
 	}
-	fmt.Println("contactList:", contactList)
 
+	poleLogList := make([]model.PoleLog, 0)
+	err = MySQL().Where("plate_number = ?", "苏E6PS87").And("start_time < ?", "2019-02-14 13:31:44").And("pay_status = 0 or status = 'IN'").Find(&poleLogList)
+	if len(poleLogList) > 0 {
+		for _, v := range poleLogList {
+			log.Println("VVV：", v)
+		}
+	}
 }
