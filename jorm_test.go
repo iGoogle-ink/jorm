aab3972b97c32c258af036adf7ab62e0ba3cb789
Jerry <jerry@iguiyu.com>
2019-08-08 15:35:30 +0800

    update

--- jorm_test.go
+++ jorm_test.go
@@ -28,7 +28,7 @@ create table contact
 
 type Contact struct {
 	UserId      int    `json:"user_id"`
-	RealName    string `json:"real_name" jorm:"real_name"`
+	RealName    string `json:"name" jorm:"real_name"`
 	Age         int    `json:"age"`
 	PhoneNumber string `json:"phone_number"`
 	HomeAddress string `json:"home_address"`
